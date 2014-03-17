9b868a1c6a9860f08816f4f10fde6b558b01ba73
dhanasiddharth <dhanasiddharth@gmail.com>
2014-03-17 11:51:14 +0530

    Merge

--- src/main/java/org/hpccsystems/dashboard/controller/EditChartController.java
+++ src/main/java/org/hpccsystems/dashboard/controller/EditChartController.java
@@ -6,8 +6,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-
-import org.apache.commons.logging.Log;
+import org.apache.commons.logging.Log; 
 import org.apache.commons.logging.LogFactory;
 import org.hpccsystems.dashboard.api.entity.ChartConfiguration;
 import org.hpccsystems.dashboard.api.entity.Field;
