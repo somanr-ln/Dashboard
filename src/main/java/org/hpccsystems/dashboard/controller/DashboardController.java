ec9b795fc4a7ae5a6d0b4399dbb38e0ab92b15cd
RajalakshmiElumalaiCts <rajalakshmi.elumalai@cognizant.com>
2014-03-17 12:02:13 +0530

    Commit issue

--- src/main/java/org/hpccsystems/dashboard/controller/DashboardController.java
+++ src/main/java/org/hpccsystems/dashboard/controller/DashboardController.java
@@ -12,7 +12,6 @@ import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.hpccsystems.dashboard.api.entity.Field;
